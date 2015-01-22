49396a8d92d0fb2be6bfd9c6ed9809f91ac7dc33
Isidro <isidrolauscher@gmail.com>
2015-01-22 13:54:26 +0100

    CRAN submission

--- conformal.Rcheck/conformal-Ex.R
+++ conformal.Rcheck/conformal-Ex.R
@@ -1,6 +1,18 @@
 pkgname <- "conformal"
 source(file.path(R.home("share"), "R", "examples-header.R"))
 options(warn = 1)
+base::assign(".ExTimings", "conformal-Ex.timings", pos = 'CheckExEnv')
+base::cat("name\tuser\tsystem\telapsed\n", file=base::get(".ExTimings", pos = 'CheckExEnv'))
+base::assign(".format_ptime",
+function(x) {
+  if(!is.na(x[4L])) x[1L] <- x[1L] + x[4L]
+  if(!is.na(x[5L])) x[2L] <- x[2L] + x[5L]
+  options(OutDec = '.')
+  format(x[1L:3L], digits = 7L)
+},
+pos = 'CheckExEnv')
+
+### * </HEADER>
 library('conformal')
 
 base::assign(".oldSearch", base::search(), pos = 'CheckExEnv')
@@ -10,6 +22,7 @@ nameEx("ConformalClassification")
 
 flush(stderr()); flush(stdout())
 
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: ConformalClassification
 ### Title: Class Conformal Prediction for Classification
 ### Aliases: ConformalClassification
@@ -33,13 +46,19 @@ LogSTest[LogSTest <= -4] <- 2
 LogSTrain <- factor(LogSTrain)
 LogSTest <- factor(LogSTest)
 
+# Remove part of the data to allow for quick training
+LogSTrain <- LogSTrain[1:20]
+LogSTest <- LogSTest[1:20]
+LogSDescsTrain <- LogSDescsTrain[1:20,]
+LogSDescsTest <- LogSDescsTest[1:20,]
+
 algorithm <- "rf"
 
 trControl <- trainControl(method = "cv",  number=5,savePredictions=TRUE)
 set.seed(3)
 
 #number of trees
-nb_trees = 100
+nb_trees <- 100
 model <- train(LogSDescsTrain, LogSTrain, 
          algorithm,type="classification", 
          trControl=trControl,predict.all=TRUE,
@@ -59,12 +78,15 @@ example$p.values$Significance_p.values
 
 
 
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("ConformalClassification", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 cleanEx()
 nameEx("ConformalRegression")
 ### * ConformalRegression
 
 flush(stderr()); flush(stdout())
 
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: ConformalRegression
 ### Title: Class ConformalRegression: Conformal Prediction for Regression
 ### Aliases: ConformalRegression
@@ -83,11 +105,17 @@ showClass("ConformalRegression")
 
 data(LogS)
 
+# Remove part of the data to allow for quick training
+LogSTrain <- LogSTrain[1:20]
+LogSTest <- LogSTest[1:20]
+LogSDescsTrain <- LogSDescsTrain[1:20,]
+LogSDescsTest <- LogSDescsTest[1:20,]
+
 algorithm <- "svmRadial"
-tune.grid <- expand.grid(.sigma = expGrid(power.from=-10, power.to=-6, power.by=1, base=2), 
-                         .C = expGrid(power.from=4, power.to=10, power.by=2, base=2))
+tune.grid <- expand.grid(.sigma = expGrid(power.from=-10, power.to=-6, power.by=2, base=2), 
+                         .C = c(1,10,100))
 trControl <- trainControl(method = "cv",  number=5,savePredictions=TRUE)
-set.seed(3)
+set.seed(1)
 model <- train(LogSDescsTrain, LogSTrain, algorithm, 
                tuneGrid=tune.grid, 
                trControl=trControl)
@@ -109,12 +137,15 @@ example$plot
 
 
 
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("ConformalRegression", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 cleanEx()
 nameEx("LogS")
 ### * LogS
 
 flush(stderr()); flush(stdout())
 
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: LogS
 ### Title: Small Molecule Solubility (LogS) Data
 ### Aliases: LogS
@@ -126,12 +157,15 @@ data(LogS)
 
 
 
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("LogS", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 cleanEx()
 nameEx("expGrid")
 ### * expGrid
 
 flush(stderr()); flush(stdout())
 
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: expGrid
 ### Title: Exponential Grid Definition
 ### Aliases: expGrid
@@ -142,6 +176,8 @@ expGrid(power.from=-10,power.to=10,power.by=2,base=10)
 
 
 
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("expGrid", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 ### * <FOOTER>
 ###
 options(digits = 7L)
