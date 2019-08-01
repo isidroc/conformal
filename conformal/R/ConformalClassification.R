6f7ca191c20320100a87a8461863485570822c2e
Isidro <isidrolauscher@gmail.com>
2019-08-01 11:45:55 +0100

    Update ConformalClassification.R

--- conformal/R/ConformalClassification.R
+++ conformal/R/ConformalClassification.R
@@ -1,7 +1,6 @@
 ## Isidro Cortes Ciriano. 2016-10-01
 ## Conformal Prediction for caret classification models
 
-
 #############################################
 ### Conformal Prediction for Classification
 #############################################
@@ -36,8 +35,18 @@ ConformalClassification <- setRefClass(
       ClassificationModel <<- model
       print("Calculating the vector of nonconformity measures for the CV predictions (label wise Mondrian ICP)..")
       cat('\n')
-      MondrianICP <- model$finalModel$votes
-      MondrianICP <- apply(MondrianICP, 2, sort, decreasing=FALSE)
+      MondrianICP <- data.frame( model$finalModel$votes )
+      names(MondrianICP) = levels(model$trainingData$.outcome)
+      # add true labels
+      #MondrianICP <- as.data.frame(MondrianICP)
+      MondrianICP$label <- as.vector( model$trainingData$.outcome )
+      # consider only the values for one class
+      for (lab in levels(model$trainingData$.outcome) ){
+        MondrianICP[which(MondrianICP$label != lab),lab] = NA
+        MondrianICP[,lab] <- sort(MondrianICP[,lab], decreasing=F,na.last = T)
+      }
+      ###MondrianICP[,1:2] <- apply(MondrianICP[,1:2], 2, sort, decreasing=FALSE)
+      MondrianICP$label=NULL
       NonconformityScoresMatrix <<- MondrianICP
     },
     CalculatePValues = function(new.data=NULL)
@@ -55,7 +64,11 @@ ConformalClassification <- setRefClass(
       pred <- predict(ClassificationModel$finalModel, newdata = new.data,predict.all=TRUE) # individual or aggregate
       ClassPredictions <<- pred
       ntrees <- ClassificationModel$finalModel$ntree
-      votes <- apply(pred$individual,1,function(x){table(x)})
+      
+      library(plyr)
+      votes <- alply(pred$individual, 1, function(x) { table(x) })
+      ##votes <- apply(pred$individual,1,function(x){table(x)})
+      
       out<-c()
       for (i in colnames(NonconformityScoresMatrix)){
         out<-cbind(out,sapply(votes,function(x) x[i]))
@@ -64,16 +77,15 @@ ConformalClassification <- setRefClass(
       out <- out/ntrees
       colnames(out) <- colnames(NonconformityScoresMatrix)
       
-      pval <- t(apply(out,1,function(x){ apply(do.call(rbind, lapply(as.data.frame(t(NonconformityScoresMatrix)), "<", x)),2,sum)    }))
+      pval <- t(apply(out,1,function(x){ apply(do.call(rbind, lapply(as.data.frame(t(NonconformityScoresMatrix)), "<", x)),2,sum,na.rm=T)    }))
       pval <- pval / nrow(NonconformityScoresMatrix)
       # this also works but is slower
       # library(plyr)
       # now <- t(apply(out,1,function(x){ apply(aaply(NonconformityScoresMatrix, 1, "<", x),2,sum)    }))
       # http://stackoverflow.com/questions/20596433/how-to-divide-each-row-of-a-matrix-by-elements-of-a-vector-in-r
       pval_signif <- (pval > (1-confidence))*1
-      #pval_signif <- (pval > confidence)*1
       p.values <<- list(P.values = pval,Significance_p.values = pval_signif)
     }
-
+    
   )
-  )
+)
